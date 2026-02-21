65f880cee40a160a36a81a1a30a59fa93837770a
Austin O'Hara <austinaohara@gmail.com>
2026-02-20 20:36:18 -0500

    Init commit

---
+++ src/main/java/edu/farmingdale/csc311_w4_lab/Launcher.java
@@ -0,0 +1,9 @@
+package edu.farmingdale.csc311_w4_lab;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
